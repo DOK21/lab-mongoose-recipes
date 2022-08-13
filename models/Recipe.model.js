2990f12217b9fcfe8d84a2130247e6f315174e82
Di <d.osseyran@hotmail.com>
2022-08-13 17:14:48 +0200

    carrying on with the lab

--- models/Recipe.model.js
+++ models/Recipe.model.js
@@ -2,9 +2,20 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const recipeSchema = new Schema({
-  // TODO: write the schema
+  title: String,
+  enum:|'Easy Peasy', 'Amateur Chef', 'UltraPro Chef'|,
+  level: String,
+  Ingredients:[string],
+  cuisine:  {type: String, required: true},
+  dishType: String,
+  role {breakfast, main_course, soup, snack, drink, dessert or other};
+  image: String,
+  duration: Number,
+  creator: String,
+  created: Date, 
 });
 
 const Recipe = mongoose.model('Recipe', recipeSchema);
 
 module.exports = Recipe;
+
